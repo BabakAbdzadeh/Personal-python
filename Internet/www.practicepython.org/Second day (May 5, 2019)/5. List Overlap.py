868cc52314079cd349b2c232a08e00ee161144b1
BabakAbdzadeh <Babak.abdzadeh@gmail.com>
2019-05-07 17:10:53 +0430

    Final Version

--- Internet/www.practicepython.org/Second day (May 5, 2019)/5. List Overlap.py
+++ Internet/www.practicepython.org/Second day (May 5, 2019)/5. List Overlap.py
@@ -3,6 +3,7 @@
 
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
 # and write a program that returns a list that contains only the elements that are common between the lists
 # (without duplicates). Make sure your program works on two lists of different sizes.
 #
@@ -33,18 +34,20 @@ def duplicate(items):
 
 a_duplicated = duplicate(a)
 b_duplicated = duplicate(b)
+last_one = b_duplicated[-1]
 
 i = -1
 b = 2
-sorted_list = []
+
 while b_duplicated and a_duplicated:
     a_min = min(a_duplicated)
+
     if b_duplicated[i] == min(a_duplicated):
         sorted_list.append(a_min)
         b_duplicated.remove(a_min)
         a_duplicated.remove(a_min)
         i = -1
-    elif a_min != b_duplicated[-1]:
+    elif a_min != last_one and i == len(b_duplicated):
         a_duplicated.remove(a_min)
         i = -1
 
