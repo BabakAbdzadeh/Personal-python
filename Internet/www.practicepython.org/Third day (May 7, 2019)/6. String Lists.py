9aff133912b9d6ec77e1c79f7f90d3202e586968
BabakAbdzadeh <Babak.abdzadeh@gmail.com>
2019-05-07 17:23:59 +0430

    Final version

--- Internet/www.practicepython.org/Third day (May 7, 2019)/6. String Lists.py
+++ Internet/www.practicepython.org/Third day (May 7, 2019)/6. String Lists.py
@@ -1,2 +1,15 @@
 # Ask the user for a string and print out whether this string is a palindrome or not.
 # (A palindrome is a string that reads the same forwards and backwards.)
+
+input_string = input("please type a string :" )
+input_list = list(input_string)
+input_list_backward = input_list[::-1]
+print(input_list_backward)
+print(input_list)
+
+if input_list == input_list_backward:
+    print("your string is palindrome!")
+else:
+    print("your string is not palindrome!")
+
+
