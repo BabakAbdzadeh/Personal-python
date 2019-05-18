a6d326da6b9f40230b1d2029b34b311d43c653b5
BabakAbdzadeh <Babak.abdzadeh@gmail.com>
2019-05-18 12:58:11 +0430

    exercise 1 ( working but i think its not the one that exercise wants!)

---
+++ Internet/www.w3resource.com/Regular Expression/First Day(18 may,2019)/exercises 1.py
@@ -0,0 +1,22 @@
+# hint :
+#
+# RegEx Functions:
+# The "re"  module offers a set of functions that allows us to search a string for a match:
+#
+# Function:	    Description:
+#  1.findall	    Returns a list containing all matches
+#  2.search	        Returns a Match object if there is a match anywhere in the string
+#  3.split	        Returns a list where the string has been split at each matchRR
+#  4.sub	        Replaces one or many matches with a string
+
+
+# 1. Write a Python program to check that a string contains only a certain set of characters (in this case a-z,
+# A-Z and 0-9)
+import re
+
+ex1_string: str = " hi my name is Babak, I am 23 years old , non SERVIAM"
+x = re.findall("[a-zA-Z0-9]", ex1_string)
+print(x)
+
+# 2. Write a Python program that matches a string that has an a followed by zero or more b's.
+
