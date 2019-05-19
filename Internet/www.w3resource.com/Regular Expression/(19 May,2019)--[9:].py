c8272b755f50835589a7107515d26e9d8a12ef87
BabakAbdzadeh <Babak.abdzadeh@gmail.com>
2019-05-19 17:42:07 +0430

    exercise 9  Works exercise 10 Works exercise 11 Works

--- Internet/www.w3resource.com/Regular Expression/(19 May,2019)--[9:].py
+++ Internet/www.w3resource.com/Regular Expression/(19 May,2019)--[9:].py
@@ -12,6 +12,7 @@ def match_string9(string):
     else:
         return 'no match found! ex9'
 
+
 print(match_string9('hi'))
 print(match_string9('a'))
 print(match_string9('ab'))
@@ -20,3 +21,41 @@ print(match_string9('aab'))
 print(match_string9('aaakjdhfskjsdb'))
 print(match_string9('himynameisb'))
 
+
+#
+#
+# 10. Write a Python program that matches a word at the beginning of a string.
+
+
+def word_match10(string):
+    pattern = '\A[^\s]*'
+    if re.search(pattern, string):
+        x = re.findall(pattern, string)
+        y = re.search(pattern, string)
+        return x, y
+    else:
+        return 'no match found! ex10'
+
+
+print(word_match10('hi my name is babak'))
+print(word_match10('himy name is babak'))
+print(word_match10('himynameisbabak'))
+
+#
+#
+# 11. Write a Python program that matches a word at end of string, with optional punctuation.
+
+
+def word_match11(string):
+    pattern = '\w+\S*$'
+    if re.search(pattern, string):
+        x = re.findall(pattern, string)
+        y = re.search(pattern, string)
+        return x, y
+    else:
+        return 'no match found! ex11'
+
+
+print(word_match11('hi my name is babak'))
+print(word_match11('hi my name is bakak.'))
+print(word_match11('hi my name is nobody else;'))
