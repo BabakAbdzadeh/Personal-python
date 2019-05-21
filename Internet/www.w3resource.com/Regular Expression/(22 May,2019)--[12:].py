6ac3cf12ec7ced221ecd821639de0432397e4679
BabakAbdzadeh <Babak.abdzadeh@gmail.com>
2019-05-22 04:21:54 +0430

    exercise 12 Works

--- Internet/www.w3resource.com/Regular Expression/(22 May,2019)--[12:].py
+++ Internet/www.w3resource.com/Regular Expression/(22 May,2019)--[12:].py
@@ -3,10 +3,16 @@ import re
 
 
 def word_match12(string):
-    pattern = ''
+    pattern = '\S*[z]\S*'
     if re.search(pattern, string):
         x = re.findall(pattern, string)
         y = re.search(pattern, string)
         print(x,y)
     else:
         print('no match found! ex12')
+
+
+print(word_match12('babak'))
+print(word_match12('zoo'))
+print(word_match12('hazard'))
+print(word_match12('in this string we have words with Z like there is a zoo in bazar'))
