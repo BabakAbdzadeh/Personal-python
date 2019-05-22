f5bed98cfb3b7ab6b0525d962e435780300346f5
BabakAbdzadeh <Babak.abdzadeh@gmail.com>
2019-05-22 04:42:02 +0430

    exercise 12 Works exercise 13 Works

--- Internet/www.w3resource.com/Regular Expression/(22 May,2019)--[12:].py
+++ Internet/www.w3resource.com/Regular Expression/(22 May,2019)--[12:].py
@@ -1,4 +1,6 @@
 import re
+
+
 # 12. Write a Python program that matches a word containing 'z'
 
 
@@ -7,7 +9,7 @@ def word_match12(string):
     if re.search(pattern, string):
         x = re.findall(pattern, string)
         y = re.search(pattern, string)
-        print(x,y)
+        print(x, y)
     else:
         print('no match found! ex12')
 
@@ -16,3 +18,20 @@ print(word_match12('babak'))
 print(word_match12('zoo'))
 print(word_match12('hazard'))
 print(word_match12('in this string we have words with Z like there is a zoo in bazar'))
+
+
+#
+# 13. Write a Python program that matches a word containing 'z', not start or end of the word.
+
+def word_match13(string):
+    pattern = '\S*\B[z,Z]\B\S*'
+    if re.search(pattern, string):
+        x = re.findall(pattern, string)
+        y = re.search(pattern, string)
+        print(x, y)
+    else:
+        print('no match found! ex13')
+
+
+print(word_match13('zoo zebra hazard lampard'
+                   'in this string, we have words with Z or oZy like there is a zoo in bazzar'))
