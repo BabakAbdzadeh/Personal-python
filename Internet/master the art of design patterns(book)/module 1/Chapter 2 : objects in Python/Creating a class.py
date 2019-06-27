8935fbf2f85708c11c07cd9fa4694be00c4c9d31
BabakAbdzadeh <Babak.abdzadeh@gmail.com>
2019-06-27 16:16:32 +0430

    module 1 , chapter 2 : creating a class : empty class + adding attributes final version

--- Internet/master the art of design patterns(book)/module 1/Chapter 2 : objects in Python/Creating a class.py
+++ Internet/master the art of design patterns(book)/module 1/Chapter 2 : objects in Python/Creating a class.py
@@ -5,4 +5,5 @@ class MyFirstClass:
 a = MyFirstClass()  # this a is a instance object
 a.x = 5     # this x is a attribute with value of 5
 a.y = 6     # this y is a attribute with value of 6
-print(a.x, a.y)
+
+print(a.x, a.y, a)
