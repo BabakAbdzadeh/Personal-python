e60fa3846922ae23f68804dfab15b5eaef9fb6c5
BabakAbdzadeh <Babak.abdzadeh@gmail.com>
2019-07-06 00:11:53 +0430

    basic inheritance Overriding and super

--- Book/master the art of design patterns(book)/module 1/Chapter 3 : When Objects Are Alike/Basic inheritance.py
+++ Book/master the art of design patterns(book)/module 1/Chapter 3 : When Objects Are Alike/Basic inheritance.py
@@ -1,5 +1,16 @@
+class ContactList(list):
+    def search(self, name):
+        '''Return all contacts that contain the search value
+                   in their name.'''
+        matching_contacts = []
+        for contact in self:
+            if name in contact.name:
+                matching_contacts.append(contact)
+        return matching_contacts
+
+
 class Contact:
-    all_contacts = []
+    all_contacts = ContactList()
 
     def __init__(self, name, email):
         self.name = name
@@ -11,6 +22,18 @@ class Supplier(Contact):
     def order(self, order):
         print("If this were a real system we would send"
               "'{}' order to '{}'".format(order, self.name))
+
+
+class Friend(Contact):
+    def __init__(self, name, email, phone):
+        super().__init__(name, email)
+        self.phone = phone
+
+
+
+
+
+
 
 
 c = Contact("Some Body", "somebody@example.net")
