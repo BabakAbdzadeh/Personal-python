612a789b7e68de541e4b185cc8ce27d553885980
BabakAbdzadeh <Babak.abdzadeh@gmail.com>
2019-07-06 11:39:16 +0430

    basic inheritance Diamond inheritance

--- Book/master the art of design patterns(book)/module 1/Chapter 3 : When Objects Are Alike/Basic inheritance.py
+++ Book/master the art of design patterns(book)/module 1/Chapter 3 : When Objects Are Alike/Basic inheritance.py
@@ -24,9 +24,18 @@ class Supplier(Contact):
               "'{}' order to '{}'".format(order, self.name))
 
 
-class Friend(Contact):
-    def __init__(self, name, email, phone):
-        super().__init__(name, email)
+class AddressHolder:
+    def __init__(self, street, city, state, code):
+        self.street = street
+        self.city = city
+        self.state = state
+        self.code = code
+
+
+class Friend(Contact, AddressHolder):
+    def __init__(self, name, email, phone, street, city, state, code):
+        Contact.__init__(self, name, email)
+        AddressHolder.__init__(self, street, city, state, code)
         self.phone = phone
 
 
@@ -41,6 +50,8 @@ class MailSender:
 
 class EmailAbleContact(Contact, MailSender):
     pass
+
+
 
 
 e = EmailAbleContact("john Smith", "jsmith@example.net")
