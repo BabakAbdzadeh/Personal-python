2d2bc03a539a4ac572943a4288b7a5458d359f5c
BabakAbdzadeh <Babak.abdzadeh@gmail.com>
2019-07-06 11:07:59 +0430

    basic inheritance multiple inheritance (mixing)

--- Book/master the art of design patterns(book)/module 1/Chapter 3 : When Objects Are Alike/Basic inheritance.py
+++ Book/master the art of design patterns(book)/module 1/Chapter 3 : When Objects Are Alike/Basic inheritance.py
@@ -1,7 +1,7 @@
 class ContactList(list):
     def search(self, name):
-        '''Return all contacts that contain the search value
-                   in their name.'''
+        """Return all contacts that contain the search value
+                   in their name."""
         matching_contacts = []
         for contact in self:
             if name in contact.name:
@@ -30,19 +30,29 @@ class Friend(Contact):
         self.phone = phone
 
 
+class MailSender:
+    def __init__(self, email):
+        self.email = email
+
+    def send_mail(self, massage):
+        print("Sending mail to " + self.email)
+        # Logic stuff
 
 
+class EmailAbleContact(Contact, MailSender):
+    pass
 
 
+e = EmailAbleContact("john Smith", "jsmith@example.net")
+e.send_mail("hi")
 
-
-c = Contact("Some Body", "somebody@example.net")
-s = Supplier("Sup Plier", "supplier@example.net")
-print(c.name, c.email, s.name, s.email)
-
-# print(c.order("I need pliers"))
-
-#  Error : AttributeError: 'Contact' object has no attribute 'order'
-
-
-print(s.order("I need pliers"))
+# c = Contact("Some Body", "somebody@example.net")
+# s = Supplier("Sup Plier", "supplier@example.net")
+# print(c.name, c.email, s.name, s.email)
+#
+# # print(c.order("I need pliers"))
+#
+# #  Error : AttributeError: 'Contact' object has no attribute 'order'
+#
+#
+# print(s.order("I need pliers"))
