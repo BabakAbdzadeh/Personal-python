cac38609109b4296c3b60793b00f48c48aeef6db
BabakAbdzadeh <Babak.abdzadeh@gmail.com>
2019-07-26 19:24:15 +0430

    real estate application - validation function

--- Book/master the art of design patterns(book)/module 1/Chapter 3 : When Objects Are Alike/real estate application.py
+++ Book/master the art of design patterns(book)/module 1/Chapter 3 : When Objects Are Alike/real estate application.py
@@ -31,7 +31,19 @@ class Property:
         return dict(square_feet=input("Enter the square feet: "),
                     beds=input("Enter number of bedrooms: "),
                     baths=input("Enter number of bathrooms: "))
+
     prompt_init = staticmethod(prompt_init)
+
+######
+# without class function #      !!!!  validation function  !!!!!!!
+
+
+def get_valid_input(input_string, valid_option):
+    input_string += "({})".format(", ".join(valid_option))
+    response = input(input_string)
+    while response.lower() not in valid_option:
+        response = input(input_string)
+    return response
 
 
 class Apartment(Property):
@@ -51,14 +63,10 @@ class Apartment(Property):
 
     def prompt_init():
         parent_init = Property.prompt_init()
-        laundry = ''
-        while laundry.lower() not in Apartment.valid_laundries:
-            laundry = input("What laundry facilities does the property have? ({})"
-                            .format(", ").join(Apartment.valid_laundries))
-        balcony = ''
-        while balcony.lower() not in Apartment.valid_balconies:
-            balcony = input("Does the property have a balcony? ({})"
-                            .format(", ".join(Apartment.valid_balconies)))
+        laundry = get_valid_input("what laundry facilities does the property have?"
+                                  , Apartment.valid_laundries)
+        balcony = get_valid_input("Does the property have a balcony?"
+                                  ,Apartment.valid_balconies)
         parent_init.update({
             "laundry": laundry,
             "balcony": balcony
