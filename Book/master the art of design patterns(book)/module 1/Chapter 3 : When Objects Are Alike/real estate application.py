4323bf9b2b7ee782ef468d3fe3e971769ebfd0cf
BabakAbdzadeh <Babak.abdzadeh@gmail.com>
2019-07-26 20:12:58 +0430

    real estate application - validation function classes : 1.property 2.Apartment 3.Houses 4.sell 5.rent

--- Book/master the art of design patterns(book)/module 1/Chapter 3 : When Objects Are Alike/real estate application.py
+++ Book/master the art of design patterns(book)/module 1/Chapter 3 : When Objects Are Alike/real estate application.py
@@ -34,6 +34,7 @@ class Property:
 
     prompt_init = staticmethod(prompt_init)
 
+
 ######
 # without class function #      !!!!  validation function  !!!!!!!
 
@@ -64,12 +65,93 @@ class Apartment(Property):
     def prompt_init():
         parent_init = Property.prompt_init()
         laundry = get_valid_input("what laundry facilities does the property have?"
-                                  , Apartment.valid_laundries)
+                                  , House.valid_garage)
         balcony = get_valid_input("Does the property have a balcony?"
-                                  ,Apartment.valid_balconies)
+                                  , House.valid_fenced)
         parent_init.update({
             "laundry": laundry,
             "balcony": balcony
         })
         return parent_init
+
     prompt_init = staticmethod(prompt_init)
+
+
+class House(Property):
+    valid_garage = ("attached", "detached", "none")
+    valid_fenced = ("yes", "no")
+
+    def __init__(self, num_stories='', garage='', fenced='', **kwargs):
+        super().__init__(**kwargs)
+        self.num_stories = num_stories
+        self.garage = garage
+        self.fenced = fenced
+
+    def display(self):
+        super().display()
+        print("HOUSE DETAILS")
+        print("# of stories: {}".format(self.num_stories))
+        print("garage: {}".format(self.garage))
+        print("fenced yard: {}".format(self.fenced))
+
+    def prompt_init():
+        parent_init = Property.prompt_init()
+        fenced = get_valid_input("Is the yard fenced?"
+                                 , House.valid_fenced)
+        garage = get_valid_input("Does the property have a garage?"
+                                 , House.valid_garage)
+        num_stories = input("How many stories? ")
+        parent_init.update({
+            "fenced": fenced,
+            "garage": garage,
+            "num_stories": num_stories
+        })
+        return parent_init
+
+    prompt_init = staticmethod(prompt_init)
+
+
+class Purchase:
+    def __init__(self, price='', taxes='', **kwargs):
+        super().__init__(**kwargs)
+        self.price = price
+        self.taxes = taxes
+
+    def display(self):
+        super().diplay()
+        print("PURCHASE DETAILS")
+        print("selling price: {}".format(self.price))
+        print("estimated taxes: {}".format(self.taxes))
+
+    def prompt_init():
+        return dict(
+            price=input("What is the selling price? "),
+            taxes=input("what are the estimated taxes?")
+        )
+    prompt_init = staticmethod(prompt_init)
+
+
+class Rental:
+    def __init__(self, rent='', furnished='', utilities='', **kwargs):
+        super().__init__(**kwargs)
+        self.furnished = furnished
+        self.utilities = utilities
+        self.rent = rent
+
+    def display(self):
+        super().diplay()
+        print("RENTAL DETAILS")
+        print("rent: {}".format(self.rent))
+        print("estimated utilities: {}".format(self.utilities))
+        print("furnished: {}".format(self.furnished))
+
+    def prompt_init():
+        return dict(
+            rent=input("What is the monthly rent? "),
+            utilities=input("What are the estimated utilities? "),
+            furnished=get_valid_input("is property furnished? ",
+                                      ("yes", "no"))
+        )
+
+    prompt_init = staticmethod(prompt_init)
+    
